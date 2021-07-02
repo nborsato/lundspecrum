d15d0fcd05454f2f2841f6769845b8828d004f38
nicholasborsato <n.borsato@gmail.com>
2021-07-02 15:59:19 +0200

    "Added the ability to manually select files."

--- lundspectrum.py
+++ lundspectrum.py
@@ -1,14 +1,30 @@
-import numpy as np
-from astropy.io import fits
+#import numpy as np
+#from astropy.io import fits
+import os
+
 import PySimpleGUI as sg
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from astropy.io import ascii
+from os import listdir, mkdir, path
+
 
 import warnings
 warnings.filterwarnings("ignore")
 
+def output_gen():
+    """Creates the output directory on the first run through>"""
+
+    directory = os.listdir()#Calls list of the directory
+
+    #Searchs if the directory "output" is in the file. If not it will make it.
+    test_element = "output"
+
+    check_for_output = np.isin(test_element,directory)
+
+    if check_for_output == False:
+        mkdir("output")
 
 
 def poly_mask(x_vals, y_vals, degree):
@@ -60,10 +76,128 @@ def salsa_data_reader(file):
 
     return data
 
-#Current file being used need to write code so the user can input their own
-file = "/Users/nicholasborsato/Documents/Lund_Files/Teaching/Labs/ASTA33/Radio lab/Test/Test_Data_2020/Obs_1.txt"
 
-data = salsa_data_reader(file)
+# Helper Functions
+#The Figure below draws the plotting area. This function was pulled from another tutorial
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+
+# \\  -------- PYPLOT -------- //
+
+
+def drawChart(data):
+    """This function plots the initial figure, taking the data as an argument."""
+
+    _VARS['pltFig'] = plt.figure()
+
+    continuum = data["Temp"].copy()
+    continuum[data["Temp"] > _VARS['continuum_height']] = np.max(data["Temp"][data["Temp"] < _VARS['continuum_height']])
+
+    plt.clf()
+    plt.plot(data["LSR"],data["Temp"], 'k', label = "data") #Plots the raw data from the file
+    #Plotting inital height of the continuum that can be canged
+    plt.plot([data["LSR"][0],data["LSR"][-1]], [_VARS['continuum_height']]*2, linestyle = "--", color = 'k',
+             label = "continuum")
+    plt.plot(data["LSR"], poly_mask(data["LSR"], continuum, _VARS['polydegree']), label="polynomial fit")
+    plt.legend()
+    _VARS['fig_agg'] = draw_figure(
+        _VARS['window']['figCanvas'].TKCanvas, _VARS['pltFig'])#Draws the figure.
+
+
+def removecontinuum(data, continuum_removal="False"):
+    """This function updates the plotted chart in the loop."""
+
+    _VARS['fig_agg'].get_tk_widget().forget()
+    plt.clf()
+    _VARS['fig_agg'] = draw_figure(
+        _VARS['window']['figCanvas'].TKCanvas, _VARS['pltFig'])
+
+    continuum = data["Temp"].copy() #Creates a copy of the temperature info
+
+    #This line removes any data above the continuum line
+    continuum[data["Temp"] > _VARS['continuum_height']] = np.max(data["Temp"][data["Temp"] < _VARS['continuum_height']])
+
+    if continuum_removal=="False": #If the user has not clicked continuum
+        plt.plot([data["LSR"][0],data["LSR"][-1]], [_VARS['continuum_height']]*2, linestyle = "--", color = 'k',
+                 label = "continuum")
+        plt.plot(data["LSR"],data["Temp"], 'k', label = "data")
+        plt.plot(data["LSR"],poly_mask(data["LSR"], continuum, _VARS['polydegree']), label = "polynomial fit")
+        plt.legend()
+
+    elif continuum_removal=="True": #Once user clicks continuum remval
+        data["Temp"] = data["Temp"] - poly_mask(data["LSR"], continuum, _VARS['polydegree'])
+        plt.plot(data["LSR"],data["Temp"], color = 'k')
+
+
+def updateData(val):
+    #If the polynomial slider is changed this will update the value
+    _VARS['polydegree'] = val
+    removecontinuum(data)
+
+def updateContinuum(val):
+    #If the continuum hieght is changed this will update the value
+    _VARS["continuum_height"] = val
+    removecontinuum(data)
+
+
+def save_files(data):
+    layout = [[sg.Text("New Window", key="new")],
+              [sg.Text('Name of the data and image file.')],
+              [sg.Text('File Name:', size =(15, 1)), sg.InputText()],
+              [sg.Submit()]]
+    window = sg.Window("Second Window", layout, modal=True)
+    choice = None
+    while True:
+        event, values = window.read()
+
+        if values != None:
+            output_gen() #Will create the output file if it does not exist.
+            file_name = "output/" + values[0] + ".csv"
+            print(file_name)
+            plt.savefig("output/" + values[0])
+            ascii.write(data,"output/" + values[0] + ".csv",overwrite=True)
+
+
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+
+    window.close()
+
+def file_selector():
+    """Function enables the user to select the txt files that they have downloaded.
+
+        returns:
+            """
+
+    layout = [[sg.Text("Choose a file: "),sg.Input(key="-IN2-" ,change_submits=True), sg.FileBrowse(key="-IN-")],
+              [sg.Button("Submit")]]
+
+    window = sg.Window("File Window", layout, modal=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+        elif event == "Submit":
+            file = values["-IN-"]
+            break
+    return file
+
+    #while True:
+        #event, values = window.read()
+
+# \\  -------- PYPLOT -------- //
+
+
+# \\  -------- PYSIMPLEGUI -------- //
+#Current file being used need to write code so the user can input their own
+
+data = salsa_data_reader(file_selector())
 
 
 
@@ -78,16 +212,6 @@ _VARS = {'window': False,
 
 plt.style.use('Solarize_Light2')
 
-# Helper Functions
-#The Figure below draws the plotting area. This function was pulled from another tutorial
-def draw_figure(canvas, figure):
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-    return figure_canvas_agg
-
-
-# \\  -------- PYSIMPLEGUI -------- //
 
 #Setting font parameters
 AppFont = 'Any 16'
@@ -145,109 +269,23 @@ _VARS['window'] = sg.Window('Random Samples',
 
 # \\  -------- PYSIMPLEGUI -------- //
 
-
-# \\  -------- PYPLOT -------- //
-
-
-def drawChart(data):
-    """This function plots the initial figure, taking the data as an argument."""
-
-    _VARS['pltFig'] = plt.figure()
-
-    continuum = data["Temp"].copy()
-    continuum[data["Temp"] > _VARS['continuum_height']] = np.max(data["Temp"][data["Temp"] < _VARS['continuum_height']])
-
-    plt.clf()
-    plt.plot(data["LSR"],data["Temp"], 'k', label = "data") #Plots the raw data from the file
-    #Plotting inital height of the continuum that can be canged
-    plt.plot([data["LSR"][0],data["LSR"][-1]], [_VARS['continuum_height']]*2, linestyle = "--", color = 'k',
-             label = "continuum")
-    plt.plot(data["LSR"], poly_mask(data["LSR"], continuum, _VARS['polydegree']), label="polynomial fit")
-    plt.legend()
-    _VARS['fig_agg'] = draw_figure(
-        _VARS['window']['figCanvas'].TKCanvas, _VARS['pltFig'])#Draws the figure.
-
-
-def updateChart(data,continuum_removal="False"):
-    """This function updates the plotted chart in the loop."""
-
-    _VARS['fig_agg'].get_tk_widget().forget()
-    plt.clf()
-    _VARS['fig_agg'] = draw_figure(
-        _VARS['window']['figCanvas'].TKCanvas, _VARS['pltFig'])
-
-    continuum = data["Temp"].copy() #Creates a copy of the temperature info
-
-    #This line removes any data above the continuum line
-    continuum[data["Temp"] > _VARS['continuum_height']] = np.max(data["Temp"][data["Temp"] < _VARS['continuum_height']])
-
-    if continuum_removal=="False": #If the user has not clicked continuum
-        plt.plot([data["LSR"][0],data["LSR"][-1]], [_VARS['continuum_height']]*2, linestyle = "--", color = 'k',
-                 label = "continuum")
-        plt.plot(data["LSR"],data["Temp"], 'k', label = "data")
-        plt.plot(data["LSR"],poly_mask(data["LSR"], continuum, _VARS['polydegree']), label = "polynomial fit")
-        plt.legend()
-
-    elif continuum_removal=="True": #Once user clicks continuum remval
-        data["Temp"] = data["Temp"] - poly_mask(data["LSR"], continuum, _VARS['polydegree'])
-        plt.plot(data["LSR"],data["Temp"], color = 'k')
-
-
-def updateData(val):
-    #If the polynomial slider is changed this will update the value
-    _VARS['polydegree'] = val
-    updateChart(data)
-
-def updateContinuum(val):
-    #If the continuum hieght is changed this will update the value
-    _VARS["continuum_height"] = val
-    updateChart(data)
-
-
-def open_window(data):
-    layout = [[sg.Text("New Window", key="new")],
-              [sg.Text('Name of the data and image file.')],
-              [sg.Text('File Name:', size =(15, 1)), sg.InputText()],
-              [sg.Submit()]]
-    window = sg.Window("Second Window", layout, modal=True)
-    choice = None
-    while True:
-        event, values = window.read()
-
-        if values != None:
-            file_name = "output/" + values[0] + ".csv"
-            print(file_name)
-            plt.savefig("output/" + values[0])
-            ascii.write(data,"output/" + values[0] + ".csv",overwrite=True)
-
-
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
-
-
-    window.close()
-
-
-
-# \\  -------- PYPLOT -------- //
-
-
 drawChart(data)
 
-# MAIN LOOP, this infinitate loop will constantly search for event which will prompt it to make changes.
 while True:
     event, values = _VARS['window'].read(timeout=200)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     elif event == 'Resample':
-        updateChart(data)
+        removecontinuum(data)
     elif event == '-Slider-':
         updateData(int(values['-Slider-']))
     elif event == '-Contfit-':
         updateContinuum(int(values['-Contfit-']))
     elif event == 'Remove Continuum':
-        updateChart(data,"True")
+        removecontinuum(data, "True")
     elif event == "open":
-        open_window(data)
+        save_files(data)
 
 _VARS['window'].close()
+
+
