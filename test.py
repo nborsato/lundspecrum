b626a1d8c0ffc839bee58177a89d60ef6bbd285e
nicholasborsato <n.borsato@gmail.com>
2021-07-05 22:19:29 +0200

    Beging to add gaussian fitting

--- test.py
+++ test.py
@@ -1,42 +1,46 @@
-#!/usr/bin/env python
+#import numpy as np
+#from astropy.io import fits
+import os
+
 import PySimpleGUI as sg
-# Demonstrates a number of PySimpleGUI features including:
-#   Default element size
-#   auto_size_buttons
-#   Button
-#   Dictionary return values
-#   update of elements in form (Text, Input)
-layout = [[sg.Text('Enter Your Passcode')],
-          [sg.Input('', size=(10, 1), key='input')],
-          [sg.Button('1'), sg.Button('2'), sg.Button('3')],
-          [sg.Button('4'), sg.Button('5'), sg.Button('6')],
-          [sg.Button('7'), sg.Button('8'), sg.Button('9')],
-          [sg.Button('Submit'), sg.Button('0'), sg.Button('Clear')],
-          [sg.Text('', size=(15, 1), font=('Helvetica', 18),
-                text_color='red', key='out')],
-          ]
-window = sg.Window('Keypad', layout,
-                   default_button_element_size=(5, 2),
-                   auto_size_buttons=False,
-                   grab_anywhere=False)
-# Loop forever reading the form's values, updating the Input field
-keys_entered = ''
-while True:
-    event, values = window.read()  # read the form
-    if event is None:  # if the X button clicked, just exit
-        break
-    if event == 'Clear':  # clear keys if clear button
-        keys_entered = ''
-    elif event in '1234567890':
-        keys_entered = values['input']  # get what's been entered so far
-        keys_entered += event  # add the new digit
-    elif event == 'Submit':
-        keys_entered = values['input']
-        window['out'].update(keys_entered)  # output the final string
-    # change the form to reflect current key string
-    window['input'].update(keys_entered)
-window.close()
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from astropy.io import ascii
+from os import listdir, mkdir, path
+from scipy import optimize
 
+file = "/Users/nicholasborsato/lundspectrum/output/test.csv"
 
+data = ascii.read(file, names =("LSR", "Temp"))
+#plt.plot(data['LSR'], data['Temp'])
+#plt.show()
 
-## Paste your code here
+def gaussian(x, height, center, width, offset):
+    return height*np.exp(-(x - center)**2/(2*width**2)) + offset
+def three_gaussians(x, h1, c1, w1, h2, c2, w2, h3, c3, w3, offset):
+    return (gaussian(x, h1, c1, w1, offset=0) +
+        gaussian(x, h2, c2, w2, offset=0) +
+        gaussian(x, h3, c3, w3, offset=0) + offset)
+
+def two_gaussians(x, h1, c1, w1, h2, c2, w2, offset):
+    return three_gaussians(x, h1, c1, w1, h2, c2, w2, 0,0,1, offset)
+
+errfunc3 = lambda p, x, y: (three_gaussians(x, *p) - y)**2
+errfunc2 = lambda p, x, y: (two_gaussians(x, *p) - y)**2
+
+guess3 = [15, 0, 20, 0.6, 0.61, 0.01, 1, 0.64, 0.01, 0]  # I guess there are 3 peaks, 2 are clear, but between them there seems to be another one, based on the change in slope smoothness there
+guess2 = [0.49, 0.55, 0.01, 1, 0.64, 0.01, 0]  # I removed the peak I'm not too sure about
+optim3, success = optimize.leastsq(errfunc3, guess3[:], args=(data["LSR"], data["Temp"]))
+optim2, success = optimize.leastsq(errfunc2, guess2[:], args=(data["LSR"], data["Temp"]))
+optim3
+
+plt.plot(data["LSR"], data["Temp"], lw=1, label='measurement')
+plt.plot(data["LSR"], three_gaussians(data["LSR"], *optim3),
+    lw=3, c='b', label='fit of 3 Gaussians')
+plt.plot(data["LSR"], two_gaussians(data["LSR"], *optim2),
+    lw=1, c='r', ls='--', label='fit of 2 Gaussians')
+plt.legend(loc='best')
+plt.show()
+
+#print(data)
